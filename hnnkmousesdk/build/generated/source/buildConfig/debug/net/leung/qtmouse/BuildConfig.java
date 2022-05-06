4166e211c15c77e071e4adff09271ef26f30493d
LiuSongHan <121513057@qq.com>
2022-05-06 13:26:16 +0800

    更新虚拟光标功能

---
+++ hnnkmousesdk/build/generated/source/buildConfig/debug/net/leung/qtmouse/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package net.leung.qtmouse;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "net.leung.qtmouse";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "net.leung.qtmouse";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 22;
+  public static final String VERSION_NAME = "22.1";
+}
